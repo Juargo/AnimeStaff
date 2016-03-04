8d8f7a24dc1bb9d1bc394ac8b46c41ca36f90ab6
Juargo <jorgeretamal.jjra@gmail.com>
2016-03-04 04:48:06 -0300

    Agregando mejora de animacion

--- controller.js
+++ controller.js
@@ -1,18 +1,111 @@
 $(document).ready(function(){
-	$(".anime-s").hover(function(){
-		
-		$(".manga-s").animate({
-			width :'24%'
-		});
-		$(this).animate({
-			width :'76%'
-		});
-		},function(){
-		$(this).animate({
-			width :'50%'
-		});
-		$(".manga-s").animate({
-			width :'50%'
-		});
+
+	$(".serie-section").hover(function(){
+		var escogido=$(this);
+			if(escogido.hasClass("anime-s")){
+				var noescogido=$(this).next();
+				var id = setInterval(framein,15);
+				var tescogido=49;
+				var tnoescogido=49;
+				function framein(){
+					if(tescogido == 70){
+						clearInterval(id);
+					}else{
+						tescogido++;
+						tnoescogido--;
+						pxescogido=tescogido + '%';
+						pxnoescogido=tnoescogido + '%';
+						escogido.css('width', pxescogido);
+						noescogido.css('width',pxnoescogido);
+					}
+				}
+				//escogido.css('width','70%');
+				//noescogido.css('width','20%')
+				//noescogido.animate({
+				//	width:"20%"
+				//})
+				//escogido.animate({
+				//	width:"70%"
+				//})
+			}else if(escogido.hasClass("manga-s")){
+				noescogido=$(this).prev();
+				var id = setInterval(framein,15);
+				var tescogido=49;
+				var tnoescogido=49;
+				function framein(){
+					if(tescogido == 70){
+						clearInterval(id);
+					}else{
+						tescogido++;
+						tnoescogido--;
+						pxescogido=tescogido + '%';
+						pxnoescogido=tnoescogido + '%';
+						escogido.css('width', pxescogido);
+						noescogido.css('width',pxnoescogido);
+					}
+				}
+				//escogido.css('width','70%');
+				//noescogido.css('width','20%')
+				//noescogido.animate({
+				//	width:"20%"
+				//})
+				//escogido.animate({
+				//	width:"70%"
+				//})
+			}
+	},function(){
+
+			var escogido=$(this);
+		if(escogido.hasClass("anime-s")){
+			var noescogido=$(this).next();
+			var id = setInterval(frameout,15);
+			var tescogido=70;
+			var tnoescogido=20;
+			function frameout(){
+				if(tescogido == 49){
+					clearInterval(id);
+				}else{
+					tescogido--;
+					tnoescogido++;
+					pxescogido=tescogido + '%';
+					pxnoescogido=tnoescogido + '%';
+					escogido.css('width', pxescogido);
+					noescogido.css('width',pxnoescogido);
+				}
+			}
+			//escogido.css('width','49%');
+			//noescogido.css('width','49%')
+			//noescogido.animate({
+			//	width:"49%"
+			//})
+			//escogido.animate({
+			//	width:"49%"
+			//})
+			}else if(escogido.hasClass("manga-s")){
+				var noescogido=$(this).prev();
+			var id = setInterval(frameout,15);
+			var tescogido=70;
+			var tnoescogido=20;
+			function frameout(){
+				if(tescogido == 49){
+					clearInterval(id);
+				}else{
+					tescogido--;
+					tnoescogido++;
+					pxescogido=tescogido + '%';
+					pxnoescogido=tnoescogido + '%';
+					escogido.css('width', pxescogido);
+					noescogido.css('width',pxnoescogido);
+				}
+			}
+				//escogido.css('width','70%');
+				//noescogido.css('width','20%')
+				//noescogido.animate({
+				//	width:"20%"
+				//})
+				//escogido.animate({
+				//	width:"70%"
+				//})
+			}
 	});
 });
