f442ba6e28bd65f35dd8727a91238d5a78fca356
Juargo <jorgeretamal.jjra@gmail.com>
2016-03-20 15:55:09 -0300

    anime json groute angularjs

--- controller.js
+++ controller.js
@@ -9,6 +9,18 @@ angular.module("AppPrincipal")
 		})
 	}
 })
+.directive("catalogoImg",function(){
+	return function(scope,element,attrs){
+		attrs.$observe('catalogoImg',function(value){
+			element.css({
+				'background-image': "url('images/"+value+"')",
+				'background-size':'cover',
+				'width': '100%',
+				'height': '100%'
+			})
+		})
+	}
+})
 .controller("HomeController",function($scope,$http){
 	$scope.datos = [];
 	$http.get("home.json")
@@ -18,10 +30,10 @@ angular.module("AppPrincipal")
 		})
 })
 .controller("CatalogoController",function($scope,$http,$routeParams){
-	$scope.animes = [];
+	$scope.globaldata = [];
 	$http.get("Data/"+$routeParams.option+".json")
 	.success(function(data){
-		$scope.animes = data;
+		$scope.globaldata = data;
 	})
 
 })
